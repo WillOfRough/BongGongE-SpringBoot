81179788c4c4fd8bc4c5c7ac1562f2a275f501a5
Lim <9guyu77@naver.com>
2020-05-15 23:42:36 +0900

    noSql Play

---
+++ src/test/java/com/example/bonggonge/BonggongeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bonggonge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonggongeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
