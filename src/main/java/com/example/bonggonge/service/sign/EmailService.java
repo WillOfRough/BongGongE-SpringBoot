dcdcc535791509b8b61340b41670a786c79dd500
Lim <9guyu77@naver.com>
2020-05-17 22:56:20 +0900

    update and jwt Fix

--- src/main/java/com/example/bonggonge/service/sign/EmailService.java
+++ src/main/java/com/example/bonggonge/service/sign/EmailService.java
@@ -4,18 +4,28 @@ package com.example.bonggonge.service.sign;
 import com.example.bonggonge.domain.entity.EmailEntity;
 import com.example.bonggonge.domain.repository.EmailRepository;
 import com.example.bonggonge.dto.EmailDto;
+import com.mongodb.WriteResult;
 import lombok.AllArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.management.Query;
 import java.util.List;
 //수정
 @Service
 @AllArgsConstructor
 public class EmailService {
+
+
+    private final MongoTemplate mongoTemplete;
 
     private final EmailRepository emailRepository;
 
@@ -54,4 +64,28 @@ public class EmailService {
 //        }
 //
 //    }
+
+
+    @Override
+    public List<ReplyDomain> insert(ReplyDomain reply) {
+        mongoTemplete.insert(reply);
+        return emailRepository.findByEmail(bno);
+    }
+
+    @Override
+    public List<ReplyDomain> update(ReplyDomain reply) {
+        int bno = reply.getBno();
+
+        ObjectId id = new ObjectId(reply.getId());
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(id));
+
+        Update update = new Update();
+        update.set("userName", reply.getUserName());
+        update.set("contents", reply.getContents());
+
+        WriteResult writeResult = mongoTemplete.updateFirst(query, update, ReplyDomain.class);
+
+        return replyRepo.findByBno(bno);
+    }
 }
